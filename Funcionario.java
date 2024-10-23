345a1f1253e9e5e5da709441d68162d559e94b1a
Raphael2549 <raphaelbatista022@gmail.com>
2024-10-23 19:23:28 -0300

    Update Funcionario.java
    Implementaçao Funcionario

--- Funcionario.java
+++ Funcionario.java
@@ -1,3 +1,25 @@
 public class Funcionario {
     
+    private String nome;
+    public int id;
+    private int cpf;
+    private String descriçao;
+    
+
+    public Funcionario(String nome, int id, int cpf, String descriçao){
+        this.nome = nome;
+        this.id = id;
+        this.cpf = cpf;
+        this.descriçao = descriçao;
+
+    
 }
+    public void retirarPizzaForno(){
+        System.out.println(this.nome + "retirou do forno");
+    }
+
+    public void entregarPizza(){
+        System.out.println(this.nome + "realizou a entrega");
+
+
+    }}
